fbb0177ed14ce1686a2a8e4a901d526ffe5cd211
Krishna Gulalia <krishnagulalia0@gmail.com>
2026-04-22 17:49:36 +0530

    33

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // important for Vercel
+  plugins: [react()],
+  base: '/',
 })
